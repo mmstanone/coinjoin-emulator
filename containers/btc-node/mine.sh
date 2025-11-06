040f32def26093897e688e2ffe5c33709f840c44
Stanislav Boboň <stanislavbobon@gmail.com>
2025-11-06 17:52:37 +0100

    fix: send txs in batches to optimize fees

--- containers/btc-node/mine.sh
+++ containers/btc-node/mine.sh
@@ -1,17 +1,41 @@
 #!/bin/sh
 
 sleep 1 # TODO make more robust by waiting for bitcoind to be ready
+curl -s -u user:password --data-binary '{"jsonrpc": "1.0", "method": "createwallet", "params": ["wallet"]}' -H 'content-type: text/plain;' http://localhost:18443 > /dev/null
 
-BLOCK_COUNT=$(curl -s -u user:password --data-binary '{"jsonrpc": "1.0", "method": "getblockcount", "params": []}' -H 'content-type: text/plain;' http://localhost:18443 | jq ".result")
-echo "Current block count: $BLOCK_COUNT"
+# Mine first 200 blocks
+ADDR=$(curl -s -u user:password --data-binary '{"jsonrpc": "1.0", "method": "getnewaddress", "params": ["wallet"]}' -H 'content-type: text/plain;' http://localhost:18443 | jq -r '.result')
+curl -s -u user:password --data-binary "{\"jsonrpc\": \"1.0\", \"method\": \"generatetoaddress\", \"params\": [201, \"$ADDR\"]}" -H 'content-type: text/plain;' http://localhost:18443 | jq
 
-if [ "$BLOCK_COUNT" = "0" ] || [ -z "$BLOCK_COUNT" ]; then
-    curl -s -u user:password --data-binary '{"jsonrpc": "1.0", "method": "createwallet", "params": ["wallet"]}' -H 'content-type: text/plain;' http://localhost:18443 > /dev/null
-
-    # Mine first 200 blocks
-    ADDR=$(curl -s -u user:password --data-binary '{"jsonrpc": "1.0", "method": "getnewaddress", "params": ["wallet"]}' -H 'content-type: text/plain;' http://localhost:18443 | jq -r '.result')
-    curl -s -u user:password --data-binary "{\"jsonrpc\": \"1.0\", \"method\": \"generatetoaddress\", \"params\": [201, \"$ADDR\"]}" -H 'content-type: text/plain;' http://localhost:18443 | jq
+# taken from https://bitcoin.stackexchange.com/a/107319
+cont=true
+smartfee=$(bitcoin-cli estimatesmartfee 6)
+if [[ "$smartfee" == *"\"feerate\":"* ]]; then
+    cont=false
 fi
+while $cont
+do
+    counterb=0
+    range=$(( $RANDOM % 11 + 20 ))
+    while [ $counterb -lt $range ]
+    do
+        power=$(( $RANDOM % 29 ))
+        randfee=`echo "scale=8; 0.00001 * (1.1892 ^ $power)" | bc`
+        newaddress=$(bitcoin-cli getnewaddress)
+        rawtx=$(bitcoin-cli createrawtransaction "[]" "[{\"$newaddress\":0.005}]")
+        fundedtx=$(bitcoin-cli fundrawtransaction "$rawtx" "{\"feeRate\": \"0$randfee\"}" | jq -r ".hex")
+        signedtx=$(bitcoin-cli signrawtransactionwithwallet "$fundedtx" | jq -r ".hex")
+        senttx=$(bitcoin-cli sendrawtransaction "$signedtx")
+        counterb=$((counterb + 1))
+        echo "Created $counterb transactions this block"
+    done
+    bitcoin-cli generatetoaddress 1 $ADDR
+    smartfee=$(bitcoin-cli estimatesmartfee 6)
+    if [[ "$smartfee" == *"\"feerate\":"* ]]; then
+        cont=false
+    fi
+done
+bitcoin-cli generatetoaddress 6 $ADDR
 
 # Mine new block periodically
 while true
